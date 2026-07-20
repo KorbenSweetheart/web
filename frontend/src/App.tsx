60f9dd4f86d89209afa408a99f395c8a3aaa7093
Daniela Cabrera <dani.cabrera22@gmail.com>
2026-07-20 23:36:47 +0300

    feat(frontend): login + register pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import LandingPage from './pages/LandingPage';
+import AppRoutes from './routes';
+import './App.css';
 
 export default function App() {
-  return <LandingPage />;
+  return <AppRoutes />;
 }
